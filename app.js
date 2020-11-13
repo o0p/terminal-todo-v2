6891aa67aaccf970bed96835660e46ec11039e52
Russell <russelldev@protonmail.com>
2020-11-13 17:53:56 +0000

    Add simplified validation

---
+++ app.js
@@ -0,0 +1,5 @@
+let input = prompt('what would you liek to do?');
+while (input !== 'quit' && input !== 'q') {
+    input = prompt('what would you liek to do?'); 
+}
+console.log('Ok, quit the app')

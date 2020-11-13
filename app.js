ee6076be2919a6408f2e46c94ad01691130460d1
Russell <russelldev@protonmail.com>
2020-11-13 21:20:18 +0000

    Add list functionality

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-let input = prompt('what would you liek to do?');
+let input = prompt('what would you like to do?');
+const todos = [1,2,3,4,5];
 while (input !== 'quit' && input !== 'q') {
+    if (input === 'l') {
+        console.log('********************************');
+        for(let i = 0; i < todos.length; i++) {
+            console.log(`${i}: ${todos[i]}`)
+        }
+        console.log('********************************');
+    }
     input = prompt('what would you liek to do?'); 
 }
 console.log('Ok, quit the app')
